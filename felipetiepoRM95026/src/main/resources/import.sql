df01df8e2f391c5541013964920634ca8aac21a2
FelipeTiepo <ftiepo20@gmail.com>
2024-04-12 17:09:43 -0300

    adicionando o banco de dados

---
+++ felipetiepoRM95026/src/main/resources/import.sql
@@ -0,0 +1,12 @@
+INSERT INTO tb_cidade(nome) VALUES('São Paulo');
+INSERT INTO tb_cidade(nome) VALUES('Campinas');
+INSERT INTO tb_cidade(nome) VALUES('Olímpia');
+INSERT INTO tb_cidade(nome) VALUES('São Roque');
+INSERT INTO tb_cidade(nome) VALUES('Ribeirão Preto');
+INSERT INTO tb_cidade(nome) VALUES('Serra Negra');
+
+INSERT INTO tb_evento(nome, data, url, cidade_id) VALUES('The Town', '15/12/2024', 'https://thetown.com.br/pt/', 1);
+INSERT INTO tb_evento(nome, data, url, cidade_id) VALUES('Festa da Vindima', '12/08/2024', 'https://turismopaulista.tur.br/evento/157_festa_da_vindima_da_vinicola_goes_em_sao_roque_#:~:text=A%20Festa%20da%20Vindima%20da,segunda%20e%20ter%C3%A7a%2Dfeira).', 4);
+INSERT INTO tb_evento(nome, data, url, cidade_id) VALUES('Passeio pela cidade', '20/06/2024', 'https://visiteserranegra.com.br/agenda-de-eventos/' 6);
+
+
