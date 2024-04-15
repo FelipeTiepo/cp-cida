3d64174153bc1c2c2d0391dcd7e3b491b1ec9790
Logon Aluno <logonrmlocal@fiap.com.br>
2024-04-15 11:55:32 -0300

    CP corrigido

--- felipes/src/main/java/br/com/fiap/eventomvc/controllers/HomeController.java
+++ felipes/src/main/java/br/com/fiap/eventomvc/controllers/HomeController.java
@@ -1,14 +1,16 @@
 package br.com.fiap.eventomvc.controllers;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+@Controller
 public class HomeController {
     //Felipe Tiepo - RM 95026
     //Felipe Adriano - RM 93015
     @RequestMapping("/")
     public String index(Model model){
-        model.addAttribute("msg", "Bem vinda ao meu checkpoint Cida S2");
-        return "/evento/index";
+        model.addAttribute("msg", "Bem vinda ao nosso checkpoint Cida S2");
+        return "index";
     }
 }
--- felipes/src/main/resources/import.sql
+++ felipes/src/main/resources/import.sql
@@ -1,12 +1,14 @@
-INSERT INTO tb_cidade(nome, estado) VALUES('São Paulo', 'sp');
-INSERT INTO tb_cidade(nome, estado) VALUES('Campinas', 'sp');
-INSERT INTO tb_cidade(nome, estado) VALUES('São Roque', 'sp');
-INSERT INTO tb_cidade(nome, estado) VALUES('Rio de Janeiro', 'rj');
-INSERT INTO tb_cidade(nome, estado) VALUES('Caldas Novas', 'go');
-INSERT INTO tb_cidade(nome, estado) VALUES('Serra Negra', 'sp');
+INSERT INTO tb_cidade(nome, estado) VALUES('São Paulo', 'SP');
+INSERT INTO tb_cidade(nome, estado) VALUES('Campinas', 'SP');
+INSERT INTO tb_cidade(nome, estado) VALUES('São Roque', 'SP');
+INSERT INTO tb_cidade(nome, estado) VALUES('Rio de Janeiro', 'RJ');
+INSERT INTO tb_cidade(nome, estado) VALUES('Caldas Novas', 'GO');
+INSERT INTO tb_cidade(nome, estado) VALUES('Serra Negra', 'SP');
 
-INSERT INTO tb_evento(nome, data, url, cidade_id) VALUES('The Town', '15122024', 'https:/tiepo', 1);
-INSERT INTO tb_evento(nome, data, url, cidade_id) VALUES('Festa da Vindima', '12082024', 'https:/cida', 4);
-INSERT INTO tb_evento(nome, data, url, cidade_id) VALUES('Passeio pela cidade', '20062024', 'https:/felipe ', 6);
+INSERT INTO tb_evento(nome, data, url, cidade_id) VALUES('The Town', '15122024', 'https://thetown.com.br', 1);
+INSERT INTO tb_evento(nome, data, url, cidade_id) VALUES('Festa da Vindima', '12082024', 'https://www.turismoflores.com.br', 4);
+INSERT INTO tb_evento(nome, data, url, cidade_id) VALUES('Passeio pela cidade', '20062024', 'www.turismoserranegra.com.br', 6);
+INSERT INTO tb_evento(nome, data, url, cidade_id) VALUES('Aniversário CIDA', '30092024', 'https://www.aniversariocida.com.br', 1);
+INSERT INTO tb_evento(nome, data, url, cidade_id) VALUES('Baile da DZ7', '20042024', 'https://www.clubdadz7.com.br', 1);
 
 
